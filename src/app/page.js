9e1a8a05bf05f727b168325c53d482a023ca25a4
Chae Roy <chaeroy55@yahoo.com>
2023-10-03 16:49:33 -0500

    create folder structure and component files

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import Image from 'next/image'
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import MenuSection from './components/MenuSection';
+import SubscribeSection from './components/SubscribeSection';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main className="">
-      
+    <main>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <MenuSection />
+      <SubscribeSection />
+      <Footer />
     </main>
   )
 }
