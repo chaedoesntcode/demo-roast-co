748bb03fe27897012de630863f23ebed8191e8a3
Chae Roy <chaeroy55@yahoo.com>
2023-10-04 08:57:10 -0500

    create hero section

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    ...nextConfig, // Extend your configuration with any existing Next.js options
+  
+    webpack(config, options) {
+      // Add the video loader configuration here
+      config.module.rules.push({
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/videos/', // Adjust the path as needed
+            outputPath: 'static/videos/', // Adjust the path as needed
+            name: '[name].[ext]',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section className="relative overflow-hidden h-screen bg-red-500">
+      <div className="absolute inset-0 w-full h-full-[100px] bg-orange-500"></div>
+      <div className='absolute inset-0 w-full h-full'>
+        <video 
+          className='object-cover h-full w-full'
+          autoPlay
+          muted
+          loop
+        >
+          <source src='video/banner-video.mp4' type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="flex flex-col text-center text-secondary">
+          <p className="text-2xl uppercase">Elevating Coffee</p>
+          <h1 className="h1 drop-shadow-md">Crafting Artisanal Coffee Excellence</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
