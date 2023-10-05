ec51eb3abb23f2b742236e0828c6dd9e5c51ed0a
Chae Roy <chaeroy55@yahoo.com>
2023-10-05 09:28:39 -0500

    add motion to subscribe section

--- src/app/components/SubscribeSection.js
+++ src/app/components/SubscribeSection.js
@@ -1,15 +1,31 @@
+'use client'
 import React from 'react'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
 
 const SubscribeSection = () => {
+
   return (
     <section className='section py-10 bg-primary'>
-      <div className='w-[85vw] text-white mx-auto flex flex-col justify-center items-center text-center gap-y-2'>
-        <h2 className='h2'>Join Our Family</h2>
-        <p className='w-1/2'>be the first to taste our latest brews, receive exclusive offers and discounts, gain access to behind-the-scenes insights</p>
-        <div className='mt-4 mb-4 w-full'>
-          <input type='email' placeholder='Email address' className='py-2 px-4 rounded-l-lg w-1/3'/>
+      <div className='max-w-[85vw] text-white mx-auto flex flex-col justify-center items-center text-center gap-y-2'>
+        <motion.h2 
+         variants={fadeIn('up', 0.1)}
+         initial="hidden"
+         whileInView={"show"}
+        className='h2'>Join Our Family</ motion.h2>
+        <motion.p 
+        variants={fadeIn('up', 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        className='w-full xl:w-1/2'>be the first to taste our latest brews, receive exclusive offers and discounts, gain access to behind-the-scenes insights</motion.p>
+        <motion.div 
+        variants={fadeIn('up', 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        className='mt-4 mb-4 w-full'>
+          <input type='email' placeholder='Email address' className='text-gray-600 py-2 px-4 focus:outline-none focus:ring rounded-l-lg w-1/2 md:w-1/3'/>
           <button className='rounded-r-lg bg-accent py-2 px-6 text-white'>Subscribe</button>
-        </div>
+        </motion.div>
       </div>
     </section>
   )
