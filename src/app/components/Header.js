e8de8f6bc51e8f03f4741f966ca6c5c7e1b48399
Chae Roy <chaeroy55@yahoo.com>
2023-10-06 15:18:26 -0500

    adjust logo size

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     <header className='bg-black text-secondary w-full fixed top-0 left-0 z-10'>
         <div className='max-w-[1300px] px-5 md:px-10 xl:px-0 mx-auto h-[100px] flex justify-between items-center'>
         {/* start: logo */}
-         <Link href='/'>
+         <Link href='/' className='w-[200px] md:w-[300px]'>
                 <Image priority={true}  src={Logo}  width={300} height={200} alt="The Demo Roast Co. Logo" />
         </Link>
         {/* end: logo */}
