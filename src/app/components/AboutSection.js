9e8ffc54817e928651a935d13adbcef33d7ca1ea
Chae Roy <chaeroy55@yahoo.com>
2023-10-06 10:49:04 -0500

    finalize about section styles

--- src/app/components/AboutSection.js
+++ src/app/components/AboutSection.js
@@ -4,16 +4,16 @@ import AboutImg from '../../../public/images/about.jpg'
 
 const AboutSection = () => {
   return (
-    <section className='section bg-secondary h-[500px]'>
-      <div className='w-[85vw] h-full mx-auto flex items-center gap-x-10'>
-        <div className='flex-1'>
-          <Image className='rounded-[30px]' src={AboutImg} width='100%' height='100%' alt="The Demo Roast Co. Logo"  />
+    <section className='section bg-secondary py-10 md:py-20'>
+      <div className='max-w-[85vw] h-full mx-auto flex flex-col gap-y-4 lg:flex-row xl:max-w-[1300px] gap-x-10'>
+        <div className='hidden md:flex flex-1'>
+          <Image className='rounded-[30px]' width={'100%'} height={'50%'} src={AboutImg} alt="The Demo Roast Co. cafe staff attending to a customer"  />
         </div>
 
         <div className='flex-1'>
           <h2 className='h2'>Learn Our Story</h2>
-          <p className='mb-6 max-w-[95%]'>Demo Roast Co. began with a deep passion for crafting exceptional coffee experiences. Founded on the principles of quality, creativity, and community, we set out to create more than just a coffee destination; we aimed to establish a place where every cup tells a story. Our mission is simple: to share our love for coffee with the world, one carefully roasted bean at a time.</p>
-          <button className='h-[50px] bg-accent hover:bg-[] px-8 py-2 rounded-lg hover:bg-[#921D1D] text-white transition'>Read More</button>
+          <p className='mb-6 lg:max-w-[85%]'>Demo Roast Co. began with a deep passion for crafting exceptional coffee experiences. Founded on the principles of quality, creativity, and community, we set out to create more than just a coffee destination; we aimed to establish a place where every cup tells a story. Our mission is simple: to share our love for coffee with the world, one carefully roasted bean at a time.</p>
+          <button className='btn'>Read More</button>
         </div>
       </div>
     </section>
--- src/variants.js
+++ src/variants.js
@@ -17,4 +17,10 @@ export const fadeIn = (direction, delay) => {
         },
       },
     };
-  };
+};
+
+export const transition = {
+    duration: 1.4,
+    ease: [0.6, 0.01, -0.05, 0.9],
+    // ease: [0.43, 0.13, 0.23, 0.96],
+};
